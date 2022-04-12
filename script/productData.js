e8860ecdd3fe671b88ee7033c90cfef20d3cf6c7
RogerOliveDelgado <rogerolivedelgado@gmail.com>
2022-04-12 12:28:55 +0200

    Main Structure

---
+++ script/productData.js
@@ -0,0 +1,50 @@
+//Product Data Information
+
+export const productData = {
+    black: {
+        path: './assets/Products/GM500-black/',
+        price: '45.99€'
+    },
+    blackyellow: {
+        path: './assets/Products/GM500-blackyellow/',
+        price: '32.99€'
+    },
+    goldenhour: {
+        path: './assets/Products/GM500-goldenhour/',
+        price: '36.99€'
+    },
+    green: {
+        path: './assets/Products/GM500-green/',
+        price: '58.99€'
+    },
+    marblehead: {
+        path: './assets/Products/GM500-marblehead/',
+        price: '78.99€'
+    },
+    moonbeam: {
+        path: './assets/Products/GM500-moonbeam/',
+        price: '16.99€'
+    },
+    naturalindigo: {
+        path: './assets/Products/GM500-naturalindigo/',
+        price: '25.99€'
+    },
+    navy: {
+        path: './assets/Products/GM500-navy/',
+        price: '36.99€'
+    },
+    teamforestgreen: {
+        path: './assets/Products/GM500-teamforestgreen/',
+        price: '88.99€'
+    },
+    true: {
+        path: './assets/Products/GM500-true/',
+        price: '45.99€'
+    },
+    white: {
+        path: './assets/Products/GM500-white/',
+        price: '67.99€'
+    },
+}
+
+
